8540ce396ab4a839f6dfefaefa82e2e01e7aaaa6
nicolas.predella <nicolas.predella@antek.it>
2024-08-21 16:39:46 +0300

    gitignore

--- property_groups.py
+++ property_groups.py
@@ -2,12 +2,14 @@ import bpy, os, tempfile, sys
 import json
 
 class PrusaPropertyGroup(bpy.types.PropertyGroup):
-    running : bpy.props.BoolProperty(name="is running", default=0) # type: ignore
-    progress : bpy.props.IntProperty(name="", min=0, max=100, default=0) # type: ignore
-    progress_text : bpy.props.StringProperty(name="") # type: ignore
+    running: bpy.props.BoolProperty(name="is running", default=0) # type: ignore
+    progress: bpy.props.IntProperty(name="", min=0, max=100, default=0) # type: ignore
+    progress_text: bpy.props.StringProperty(name="") # type: ignore
     stop_process: bpy.props.BoolProperty(name="stop", default=0) # type: ignore
 
-    config : bpy.props.StringProperty(
+    config: bpy.props.StringProperty(
         name="PrusaSlicer Configuration (.ini)",  # type: ignore
         subtype='FILE_PATH'
     )
+
+    
